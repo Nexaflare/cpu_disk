93e4650c73698a0fae2ca60b598a28f7fe31f5e2
Aleksey Kim <alex.romanovich.ak@gmail.com>
2024-01-29 13:59:46 +0900

    connected power mod to disk mod

--- src/cpu/cpu.service.ts
+++ src/cpu/cpu.service.ts
@@ -3,4 +3,9 @@ import { PowerService } from '../power/power.service';
 @Injectable()
 export class CpuService {
   constructor(private powerService: PowerService) {} // before creating the cpu service, nest will create an instance of the power service and provide it to the cpu constructor
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from PowerService');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
