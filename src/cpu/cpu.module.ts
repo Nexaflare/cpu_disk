7134d6009bda0c342f3c13c64a3f0e90c8ebd055
Aleksey Kim <alex.romanovich.ak@gmail.com>
2024-01-29 16:12:54 +0900

    completed the bottom up di module connection and tested the requests

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -5,5 +5,7 @@ import { PowerModule } from '../power/power.module';
 @Module({
   imports: [PowerModule], // Step 2: connected the power module to the cpu module
   providers: [CpuService],
+  // by default services are private inside the @Module unless we write them in the 'exports'
+  exports: [CpuService],
 })
 export class CpuModule {}
